fdb35e223419610d0a3bbd79850a752e70da34a7
budarov <budarov@haulmont.com>
2021-09-22 13:46:05 +0400

    Manual - Chapter CurrentAuthentication API

--- src/main/java/com/company/jmixpm/screen/project/ProjectEdit.java
+++ src/main/java/com/company/jmixpm/screen/project/ProjectEdit.java
@@ -1,10 +1,20 @@
 package com.company.jmixpm.screen.project;
 
+import com.company.jmixpm.entity.User;
+import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.ui.screen.*;
 import com.company.jmixpm.entity.Project;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Project.edit")
 @UiDescriptor("project-edit.xml")
 @EditedEntityContainer("projectDc")
 public class ProjectEdit extends StandardEditor<Project> {
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<Project> event) {
+        event.getEntity().setManager((User) currentAuthentication.getUser());
+    }
 }
