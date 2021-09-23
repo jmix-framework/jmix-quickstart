80588b98c9ceaa11bacf74e095f6e3cb73eb7e17
Andrey Belyaev <belyaev.andrey@gmail.com>
2021-09-23 19:22:25 +0300

    Fixed instance name display for task and added test data

--- src/main/java/com/company/jmixpm/entity/Task.java
+++ src/main/java/com/company/jmixpm/entity/Task.java
@@ -1,6 +1,7 @@
 package com.company.jmixpm.entity;
 
 import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
 import io.jmix.core.metamodel.annotation.JmixEntity;
 
 import javax.persistence.*;
@@ -17,6 +18,7 @@ public class Task {
     @Id
     private UUID id;
 
+    @InstanceName
     @Column(name = "NAME", nullable = false)
     @NotNull
     private String name;
