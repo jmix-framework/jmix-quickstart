00f67168dd223aaddc988e3c1813ce546066e367
budarov <budarov@haulmont.com>
2021-10-28 19:05:25 +0400

    upgrade to jmix 1.1.0

--- src/main/java/com/company/jmixpm/security/RestrictedProjectsRole.java
+++ src/main/java/com/company/jmixpm/security/RestrictedProjectsRole.java
@@ -4,17 +4,16 @@ import com.company.jmixpm.entity.Project;
 import com.company.jmixpm.entity.User;
 import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.security.model.RowLevelPolicyAction;
+import io.jmix.security.model.RowLevelPredicate;
 import io.jmix.security.role.annotation.PredicateRowLevelPolicy;
 import io.jmix.security.role.annotation.RowLevelRole;
-
-import java.util.function.Predicate;
 
 @RowLevelRole(name = "Restrict projects for modification", code = "restricted-projects")
 public interface RestrictedProjectsRole {
     @PredicateRowLevelPolicy(entityClass = Project.class,
             actions = {RowLevelPolicyAction.UPDATE, RowLevelPolicyAction.DELETE}
     )
-    default Predicate<Project> allowOnlyManagerUpdateOrDeleteProject(CurrentAuthentication currentAuthentication) {
+    default RowLevelPredicate<Project> allowOnlyManagerUpdateOrDeleteProject(CurrentAuthentication currentAuthentication) {
         return project -> {
             User currentUser = (User) currentAuthentication.getUser();
             return currentUser.equals(project.getManager());
