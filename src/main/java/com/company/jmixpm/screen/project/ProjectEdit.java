60c85d1d39bb8fd86e320f5622d8ac5697c438af
budarov <budarov@haulmont.com>
2021-11-08 02:20:45 +0400

    substitution usage sample

--- src/main/java/com/company/jmixpm/screen/project/ProjectEdit.java
+++ src/main/java/com/company/jmixpm/screen/project/ProjectEdit.java
@@ -4,7 +4,6 @@ import com.company.jmixpm.entity.Project;
 import com.company.jmixpm.entity.User;
 import io.jmix.core.DataManager;
 import io.jmix.core.security.CurrentAuthentication;
-import io.jmix.core.usersubstitution.CurrentUserSubstitution;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,16 +15,10 @@ public class ProjectEdit extends StandardEditor<Project> {
     private CurrentAuthentication currentAuthentication;
     @Autowired
     private DataManager dataManager;
-    @Autowired
-    private CurrentUserSubstitution currentUserSubstitution;
 
     @Subscribe
     public void onInitEntity(InitEntityEvent<Project> event) {
         User user = (User) currentAuthentication.getUser();
-        if (currentUserSubstitution.getSubstitutedUser() != null) {
-            user = (User) currentUserSubstitution.getSubstitutedUser();
-        }
-
         // todo delete when jmix bug is fixed
         user = dataManager.load(User.class).id(user.getId()).one();
 
